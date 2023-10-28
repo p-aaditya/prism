9640f3cf6139227baaa3ea6b29c7a93cc5047f21
p-aaditya <aaditya-pattabhiraman@proton.me>
2023-10-28 13:03:28 +0530

    ok

--- mod.py
+++ mod.py
@@ -2,16 +2,22 @@ from tensorflow.keras.models import load_model
 import tensorflow as tf 
 from keras.preprocessing.image import load_img, img_to_array 
 import cv2
+import glob
 
-model = load_model("/home/akugyo/hakunamatata.h5")
+model = load_model("/home/akugyo/Documents/GitHub/prism/hakunamatata0.h5")
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 model.summary()
-img = tf.keras.utils.load_img("/home/akugyo/Downloads/SharetoStudent/mfhdrinput_20221028171157_1_cam0_ev-2.00_0_4000x3060.jpg")
-img = img.resize((256, 256))
-img = img_to_array(img) 
-img = img.reshape( -1,256, 256,3)
-y_pred=model.predict(img)
-heh=max(enumerate(y_pred[0]),key=lambda x: x[1])[0]
-print(y_pred)
+
+data = "/home/akugyo/Prism/Sample/*"
+
+for images in glob.glob(data):
+    img = tf.keras.utils.load_img(images)
+    img = img.resize((256, 256))
+    img = img_to_array(img) 
+    img = img.reshape( -1,256, 256,3)
+    y_pred=model.predict(img)
+    heh=tf.nn.softmax(y_pred[0])
+    if (heh[0] + heh[1] > 0.40):
+        print(images)
